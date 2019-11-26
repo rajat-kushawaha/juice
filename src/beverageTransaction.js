b2c6a2526d028383bdccdf92349257a872fd4477
= <=>
2019-11-26 22:53:07 +0530

    juice assignment stage 2 working

---
+++ src/beverageTransaction.js
@@ -0,0 +1,36 @@
+const tranctionUtility = require("./transactionUtility");
+const utils = require("./utils");
+const fs = require("fs");
+const parseInput = require("./parseInput").parseInput;
+
+const performTransaction = function(
+  empBeverageEntry,
+  readerFunc,
+  existFileFunc
+) {
+  const beverageEntry = parseInput(empBeverageEntry);
+  const transactionType = beverageEntry.command;
+  const transactionPerformer = tranctionUtility[transactionType];
+  const fileName = utils.getDataFileName();
+  const currentTime = new Date();
+  const empBeverageRecords = utils.getTransactions(
+    fileName,
+    readerFunc,
+    existFileFunc
+  );
+  const transactionResponse = transactionPerformer(
+    empBeverageRecords,
+    beverageEntry,
+    currentTime
+  );
+  if (transactionType == "save") {
+    utils.updateTransaction(transactionResponse.empBeverageRecords, fileName);
+    return utils.createConfirmMessage(
+      transactionResponse.beverageEntry,
+      currentTime
+    );
+  }
+  return utils.formatOutputData(transactionResponse);
+};
+
+exports.performTransaction = performTransaction;
